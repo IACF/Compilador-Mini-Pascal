e89a5461d7d9ae89ba4fd3bfbd634356150c87c1
Victor Gabriel <victorgfb80@gmail.com>
2018-11-27 09:58:57 -0300

    Adiciona construtor

--- src/Token.java
+++ src/Token.java
@@ -1,10 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Token {
     public byte kind;
     public String spelling;
+    public int line;
+    public int collum;
 
-    public Token(byte kind, String spelling) {
+    public Token(byte kind, String spelling, int line, int collum) {
         this.kind = kind;
         this.spelling = spelling;
+        this.line = line;
+        this.collum = collum;
+
+        Map<String, String> map = new HashMap<>();
         
 
     }
