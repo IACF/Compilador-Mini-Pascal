e95027e4c2831264d4129faaff7dcd5ca264751b
italo.andre2007 <italo.andre2007@gmail.com>
2018-11-27 09:51:10 -0300

    Constructor

--- src/Token.java
+++ src/Token.java
@@ -1,2 +1,11 @@
 public class Token {
+    public byte kind;
+    public String spelling;
+
+    public Token(byte kind, String spelling) {
+        this.kind = kind;
+        this.spelling = spelling;
+        
+
+    }
 }
