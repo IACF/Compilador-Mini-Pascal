08f6504c769eeccdaf25075cdfde246afde3359e
Victor Gabriel <victorgfb80@gmail.com>
2019-01-10 09:41:04 -0300

    Adiciona tratamento de erros de sintaxe

--- compilador/src/compilador/Parser.java
+++ compilador/src/compilador/Parser.java
@@ -46,6 +46,7 @@ public class Parser {
             parseSeletor();
             accept(":=");
             parseExpressao();
+            return;
         }else{
             if(scanner.map.get("if") == currentToken.kind){
                 acceptIt();
@@ -56,18 +57,24 @@ public class Parser {
                     acceptIt();
                     parseComando();
                 }
+                return;
             }else{
                 if(scanner.map.get("while") == currentToken.kind){
                     acceptIt();
                     parseExpressao();
                     accept("do");
                     parseComando();
+                    return;
                 }else{
-                    if(scanner.map.get("begin") == currentToken.kind)
+                    if(scanner.map.get("begin") == currentToken.kind){
                         parseComandoComposto();
+                        return;
+                    }
                 }
             } 
         }
+        
+      throw new Error(currentToken);
    }
     
     private void parseSeletor() throws IOException {
--- compilador/src/compilador/Token.java
+++ compilador/src/compilador/Token.java
@@ -1,6 +1,5 @@
 package compilador;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Token {
